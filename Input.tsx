014f39d821c67f764b9957ff447bf0daf6a1dd27
Eyob94 <eyobsh123@gmail.com>
2022-10-18 07:24:45 +0300

    committed

--- Input.tsx
+++ Input.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Text, View } from "react-native";
 import { TextInput } from "react-native-paper";
 
-const Input = (props) => {
+const Input = (props: any) => {
 	return (
 		<View>
 			<TextInput {...props} />
